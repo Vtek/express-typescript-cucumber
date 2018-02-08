568c764fef625b1e14619cfd7d95556c6360a08b
Vtek <pontoreau.sylvain@gmail.com>
2018-02-08 22:42:31 +0100

    Update to Cucumber 4

--- calculator/features/step_definitions/add.steps.ts
+++ calculator/features/step_definitions/add.steps.ts
@@ -1,28 +1,25 @@
 import { expect } from "chai";
-import { defineSupportCode, World } from "cucumber";
+import { Given, When, Then } from 'cucumber'
 import { Calculator } from "../../src/calculator";
 
-defineSupportCode(({ Given, When, Then }) => {
+const context = {
+    calculator: null,
+    result: 0
+};
 
-    const context = {
-        calculator: null,
-        result: 0
-    }
+Given("a calculator", () => {
+    context.calculator = new Calculator();
+    return;
+});
 
-    Given("a calculator", () => {
-        context.calculator = new Calculator();
-        return;
-    });
+When("I add {int} and {int}", (number1, number2) => {
+    const calculator = context.calculator as Calculator;
+    context.result = calculator.Add(<number>number1, <number>number2);
+    return;
+});
 
-    When("I add {int} and {int}", (number1, number2) => {
-        const calculator = context.calculator as Calculator;
-        context.result = calculator.Add(<number>number1, <number>number2);
-        return;
-    });
-
-    Then("the result is {int}", (result) => {
-        const actual = context.result;
-        expect(actual).be.equal(result);
-        return;
-    });
+Then("the result is {int}", (result) => {
+    const actual = context.result;
+    expect(actual).be.equal(result);
+    return;
 });
